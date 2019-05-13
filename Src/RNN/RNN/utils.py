c125284034c12b47258666ea9ddfe1032e4106cb
tan.le <leminhtanvatc@outlook.com>
2019-05-13 13:14:19 +0700

    Done project.

---
+++ Src/RNN/RNN/utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def softmax(x):
+    xt = np.exp(x - np.max(x))
+    return xt / np.sum(xt)
+
+def save_model_parameters_theano(outfile, model):
+    U, V, W = model.U.get_value(), model.V.get_value(), model.W.get_value()
+    np.savez(outfile, U=U, V=V, W=W)
+    print "Saved model parameters to %s." % outfile
+   
+def load_model_parameters_theano(path, model):
+    npzfile = np.load(path)
+    U, V, W = npzfile["U"], npzfile["V"], npzfile["W"]
+    model.hidden_dim = U.shape[0]
+    model.word_dim = U.shape[1]
+    model.U.set_value(U)
+    model.V.set_value(V)
+    model.W.set_value(W)
+    print "Loaded model parameters from %s. hidden_dim=%d word_dim=%d" % (path, U.shape[0], U.shape[1])
+
+def generate_sentence(sentence_start_token, sentence_end_token, word_to_index, index_to_word, unknown_token, model):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index[sentence_start_token]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+        new_sentence.append(sampled_word)
+        if len(new_sentence) > 30:
+            break
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
